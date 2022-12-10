32624b099fda8a839d02a0b4b9590c437e3c9415
Poul <poul.pheez2@gmail.com>
2022-12-09 23:07:33 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/88110640/json/')
-.then(resposta => resposta.json())
+.then(resposta => resposta.json())// converte a resposta do fetch para json
 .then(r => {
     if(r.erro){
+        // throw insere a mensagem de erro, quase igual a um return
         throw Error('Ta errado o CEP bixo!');
     }else{
+        // se não houver erro só segue o baile 
         console.log(r);
     }
     })
