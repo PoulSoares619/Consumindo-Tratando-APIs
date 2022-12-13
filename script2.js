8b8463c6f089026e76cb96bd3d6677125192a010
Poul <poul.pheez2@gmail.com>
2022-12-13 11:47:23 -0300

    Promise.all

--- script2.js
+++ script2.js
@@ -1,13 +1,17 @@
-async function buscaEndereco() {
+async function buscaEndereco(CEP) {
     try{
-        var consultaCEP = await fetch('https://viacep.com.br/ws/88110640/json/');
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${CEP}/json/`);
         var consultaCEPConvertido = await consultaCEP.json();
         if(consultaCEPConvertido.erro){
             throw Error('CEP não existente.');
         }
-        console.log(consultaCEPConvertido)
+        console.log(consultaCEPConvertido); 
     } catch(erro) {
         console.log(erro);
     }
 }
 
+
+var ceps = ['93420090','91790010'];
+var conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
