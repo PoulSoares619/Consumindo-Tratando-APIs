f2226edf68f501bcc872a31e79bf9b00050ae664
Poul <poul.pheez2@gmail.com>
2023-01-08 23:44:52 -0300

    .

--- script.js
+++ script.js
@@ -16,4 +16,6 @@ var consultaCEP = fetch('https://viacep.com.br/ws/88110640/json/')
 .catch(erro => console.log(erro))
 .finally(concluido => console.log('Processo concluido.'));
 
+//estudar isso segunda/ projeto pessoal
+
 
